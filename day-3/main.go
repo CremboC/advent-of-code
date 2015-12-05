34de0c98f1889ffb302a980b5ec94a87436448e9
crembo <cremboc@gmail.com>
2015-12-05 19:52:56 +0000

    Second star for Day 3

--- day-3/main.go
+++ day-3/main.go
@@ -15,31 +15,21 @@ func main() {
 	const max = 10000
 	var houses [max][max]int
 	var x, y int = max / 2, max / 2
+	var x1, y1 int = max / 2, max / 2
 
 	houses[x][y] = 1
 
 	for i := 0; i < len(data); i++ {
 		move := string(data[i])
-		fmt.Printf("Movement: %s", move)
-		fmt.Println()
 
-		if move == "v" {
-			y--
-		}
-
-		if move == "^" {
-			y++
-		}
-
-		if move == ">" {
-			x++
-		}
-
-		if move == "<" {
-			x--
+		if i % 2 == 0 {
+			x, y = change(x, y, move)
+		} else {
+			x1, y1 = change(x1, y1, move)
 		}
 
 		houses[x][y]++
+		houses[x1][y1]++
 	}
 
 	var visited int
@@ -53,4 +43,24 @@ func main() {
 
 	fmt.Printf("Visited: %d", visited)
 	fmt.Println()
+}
+
+func change(x, y int, move string) (int, int) {
+	if move == "v" {
+		y--
+	}
+
+	if move == "^" {
+		y++
+	}
+
+	if move == ">" {
+		x++
+	}
+
+	if move == "<" {
+		x--
+	}
+
+	return x, y
 }
