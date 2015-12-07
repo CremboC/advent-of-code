2531c1e810baed08d5e0dbb447569eda75bf82c5
crembo <cremboc@gmail.com>
2015-12-07 19:04:44 +0000

    First star for day 7

--- day-7/main.go
+++ day-7/main.go
@@ -11,75 +11,140 @@ var opts = []string{
 	"AND", "OR", "LSHIFT", "RSHIFT", "NOT",
 }
 
+type Op struct {
+	op1, op2, operator, target string
+}
+
 var registers = make(map[string]uint16)
+var operations = make(map[string][]Op)
 
 func main() {
-	contents, err := ioutil.ReadFile("test.data")
+	contents, err := ioutil.ReadFile("input.data")
 	if err != nil {
 		panic(err)
 	}
 	cmds := strings.Split(string(contents), "\n")
+	operations = make(map[string][]Op, len(cmds))
 
 	for _, cmd := range cmds {
-		left, operator, target := decon(cmd)
-
-		switch operator {
-		case "":
-			val, ok := parseInt(left)
-			if !ok {
-				f := strings.TrimSpace(left)
-				registers[target] = registers[f]
-			} else {
-				registers[target] = val
-			}
-		case "NOT":
-			sp := strings.Split(left, "NOT ")
-			registers[target] = ^registers[sp[1]]
-		default:
-			l, r := split(left, operator)
-			switch operator {
-			case "AND":
-				lval, lok := parseInt(l)
-				rval, rok := parseInt(r)
-
-				switch {
-				case rok && lok:
-					registers[target] = lval & rval
-				case !rok && lok:
-					registers[target] = lval & registers[r]
-				case rok && !lok:
-					registers[target] = registers[l] & rval
-				case !rok && !lok:
-					registers[target] = registers[l] & registers[r]
-				}
-
-			case "OR":
-				lval, lok := parseInt(l)
-				rval, rok := parseInt(r)
-
-				switch {
-				case rok && lok:
-					registers[target] = lval | rval
-				case !rok && lok:
-					registers[target] = lval | registers[r]
-				case rok && !lok:
-					registers[target] = registers[l] | rval
-				case !rok && !lok:
-					registers[target] = registers[l] | registers[r]
-				}
-			case "LSHIFT":
-				val, _ := parseInt(r)
-				registers[target] = registers[l] << val
-			case "RSHIFT":
-				val, _ := parseInt(r)
-				registers[target] = registers[l] >> val
-			}
-		}
+		op1, op2, operator, target := decon(cmd)
+		val, _ := operations[target]
+		operations[target] = append(val, Op{op1, op2, operator, target})
 	}
 
-	fmt.Println(registers)
+	calculate("a")
+
+	// fmt.Println(registers)
 	fmt.Printf("Register a: %d", registers["a"])
 	fmt.Println()
+}
+
+func calculate(target string) {
+	ops := operations[target]
+
+	var o Op
+	if len(ops) > 0 {
+		o = ops[0]
+		operations[target] = append(ops[:0], ops[1:]...)
+	}
+
+	switch o.operator {
+	case "NOT": not(o)
+	case "LSHIFT": lshift(o)
+	case "RSHIFT": rshift(o)
+	case "OR": or(o)
+	case "AND": and(o)
+	default: def(o)
+	}
+}
+
+func def(o Op) {
+	var lval uint16
+	var lok bool
+
+	i, err := strconv.ParseInt(o.op1, 10, 16)
+	if err == nil {
+		lval = uint16(i)
+		lok = true
+	} else {
+		lval, lok = registers[o.op1]
+	}
+
+	if !lok {
+		calculate(o.op1)
+		lval = registers[o.op1]
+	}
+	registers[o.target] = lval
+}
+
+func not(o Op) {
+	val, ok := registers[o.op1]
+	if !ok {
+		calculate(o.op1)
+		val = registers[o.op1]
+	}
+	registers[o.target] = ^val
+}
+
+func lshift(o Op) {
+	val, ok := registers[o.op1]
+	if !ok {
+		calculate(o.op1)
+		val = registers[o.op1]
+	}
+	registers[o.target] = val << parseInt(o.op2)
+}
+
+func rshift(o Op) {
+	val, ok := registers[o.op1]
+	if !ok {
+		calculate(o.op1)
+		val = registers[o.op1]
+	}
+	registers[o.target] = val >> parseInt(o.op2)
+}
+
+func and(o Op) {
+	var lval uint16
+	var lok bool
+
+	i, err := strconv.ParseInt(o.op1, 10, 16)
+	if err == nil {
+		lval = uint16(i)
+		lok = true
+	} else {
+		lval, lok = registers[o.op1]
+	}
+	rval, rok := registers[o.op2]
+
+	if !lok {
+		calculate(o.op1)
+		lval = registers[o.op1]
+	}
+
+	if !rok {
+		calculate(o.op2)
+		rval = registers[o.op2]
+	}
+
+	registers[o.target] = lval & rval
+}
+
+func or(o Op) {
+	lval, lok := registers[o.op1]
+	rval, rok := registers[o.op2]
+
+	if !lok {
+		calculate(o.op1)
+		lval = registers[o.op1]
+	}
+
+	if !rok {
+		calculate(o.op2)
+		rval = registers[o.op2]
+	}
+
+	registers[o.target] = lval | rval
 }
 
 func split(left, operator string) (string, string) {
@@ -87,9 +152,8 @@ func split(left, operator string) (string, string) {
 	return sp[0], sp[1]
 }
 
-func decon(cmd string) (string, string, string) {
+func decon(cmd string) (op1, op2, operator, target string) {
 	sp := strings.Split(cmd, " -> ")
-	operator := ""
 	for _, op := range opts {
 		if strings.Contains(cmd, op) {
 			operator = op
@@ -97,10 +161,27 @@ func decon(cmd string) (string, string, string) {
 		}
 	}
 
-	return sp[0], operator, sp[1]
+	left := strings.Split(sp[0], " ")
+	switch operator {
+	case "NOT":
+		op1 = left[1]
+	case "LSHIFT":
+		fallthrough
+	case "RSHIFT":
+		fallthrough
+	case "OR":
+		fallthrough
+	case "AND":
+		op1, op2 = left[0], left[2]
+	default:
+		op1 = left[0]
+	}
+	target = sp[1]
+
+	return
 }
 
-func parseInt(val string) (uint16, bool) {
-	i, err := strconv.ParseInt(val, 10, 16)
-	return uint16(i), err == nil
+func parseInt(val string) uint16 {
+	i, _ := strconv.ParseInt(val, 10, 16)
+	return uint16(i)
 }
