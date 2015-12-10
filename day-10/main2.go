04a62211f0eb2285a049487590e266e36ecd8dd8
crembo <cremboc@gmail.com>
2015-12-10 14:23:11 +0000

    Day 10 second star

---
+++ day-10/main2.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const times = 50
+
+func main() {
+	input := []byte{1, 1, 1, 3, 2, 2, 2, 1, 1, 3}
+	var n []byte
+	var l int
+	var t byte
+
+	for i := 0; i < times; i++ {
+		n = make([]byte, 0)
+		l = len(input)
+		t = 1
+		for j := 1; j < l; j++ {
+			if input[j - 1] == input[j] {
+				t++
+			} else {
+				n = append(n, t, input[j - 1])
+				t = 1
+			}
+		}
+		n = append(n, t, input[l - 1])
+
+		input = n
+	}
+
+	fmt.Println(len(input))
+}
