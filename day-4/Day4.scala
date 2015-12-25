96127d385a70103e6280bb794d9cf02322e6a6bb
crembo <cremboc@gmail.com>
2015-12-26 00:26:39 +0200

    Day 4 better solution

--- day-4/Day4.scala
+++ day-4/Day4.scala
@@ -1,26 +1,26 @@
 import java.security.MessageDigest
 
+import scala.annotation.tailrec
+
 /**
   * Created by paulius on 17/12/2015.
   */
 
-object Day4 {
-  val m = MessageDigest.getInstance("MD5")
+val m = MessageDigest.getInstance("MD5")
+def md5(s: String) = {
+  m.update(s.getBytes)
+  m.digest().map("%02x".format(_)).mkString
+}
 
-  def md5(s: String) = {
-    m.update(s.getBytes)
-    m.digest().map("%02x".format(_)).mkString
-  }
+val original = "iwrupvqb"
 
-  def main(args: Array[String]) {
-    val original = "iwrupvqb"
-    Stream.from(1).foreach(i => {
-      val input = original + i
-      if (md5(input).startsWith("000000")) {
-        println(i)
-        return
-      }
-    })
+@tailrec
+def calc(input: String, number: Int): Int = {
+  if (md5(input).startsWith("00000")) number - 1
+  else {
+    calc(original + number, number + 1)
   }
 }
+
+println(calc(original, 1))
 
