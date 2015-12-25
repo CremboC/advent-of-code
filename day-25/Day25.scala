8ba5e94564185eb9acc3c95edca0575c4afaa5ae
crembo <cremboc@gmail.com>
2015-12-25 12:53:53 +0200

    Day 25 shorter code

--- day-25/Day25.scala
+++ day-25/Day25.scala
@@ -1,22 +1,17 @@
 import scala.annotation.tailrec
 
-class Point(val y: Int, val x: Int) {
-  override def equals(other: Any): Boolean = other match {
-    case that: Point => y == that.y && x == that.x
-    case _ => false
-  }
-}
+case class Point(y: Int, x: Int)
 
-val search = new Point(2981, 3075) // row, column; y, x
+val search = Point(2981, 3075) // row, column; y, x
 def nextCode(prev: Long): Long = prev * 252533L % 33554393L
 
 @tailrec
 def find(coord: Point, yBegin: Int, previousCode: Long): Long = {
   if (coord == search) previousCode
   else {
-    if (coord.x == yBegin) find(new Point(yBegin + 1, 1), yBegin + 1, nextCode(previousCode))
-    else find(new Point(coord.y - 1, coord.x + 1), yBegin, nextCode(previousCode))
+    if (coord.x == yBegin) find(Point(yBegin + 1, 1), yBegin + 1, nextCode(previousCode))
+    else find(Point(coord.y - 1, coord.x + 1), yBegin, nextCode(previousCode))
   }
 }
 
-println(find(new Point(1, 1), 1, 20151125))
+println(find(Point(1, 1), 1, 20151125))
