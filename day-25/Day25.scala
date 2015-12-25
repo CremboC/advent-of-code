1ef82da4cac51993469a43ea99f2836820454f5d
crembo <cremboc@gmail.com>
2015-12-25 12:55:21 +0200

    Day 25 even shorter code

--- day-25/Day25.scala
+++ day-25/Day25.scala
@@ -8,10 +8,8 @@ def nextCode(prev: Long): Long = prev * 252533L % 33554393L
 @tailrec
 def find(coord: Point, yBegin: Int, previousCode: Long): Long = {
   if (coord == search) previousCode
-  else {
-    if (coord.x == yBegin) find(Point(yBegin + 1, 1), yBegin + 1, nextCode(previousCode))
-    else find(Point(coord.y - 1, coord.x + 1), yBegin, nextCode(previousCode))
-  }
+  else if (coord.x == yBegin) find(Point(yBegin + 1, 1), yBegin + 1, nextCode(previousCode))
+  else find(Point(coord.y - 1, coord.x + 1), yBegin, nextCode(previousCode))
 }
 
 println(find(Point(1, 1), 1, 20151125))
